145466886b687789f6947f46e33233a3006be837
Taylor Somers <tsomers@yandex.com>
2020-06-26 12:37:18 -0400

    Add correctly functioning mercurialAge prototype method and pass test for said prototype method

--- src/super-galactic-age-calculator.js
+++ src/super-galactic-age-calculator.js
@@ -2,4 +2,10 @@ export class GalacticAge {
   constructor(terrestrialAge) {
     this.terrestrialAge = terrestrialAge;
   }
+
+  mercurialAge() {
+    let mercurialAge = this.terrestrialAge*4.17
+    return mercurialAge;
+  }
+
 }
