f5dca6a6d02484fdf7a88de3c75f89cf8db4a335
Taylor Somers <tsomers@yandex.com>
2020-06-26 14:43:29 -0400

    Add functional terrestrialYearsLeft prototype method and pass test for said prototype method

--- src/super-galactic-age-calculator.js
+++ src/super-galactic-age-calculator.js
@@ -3,29 +3,35 @@ export class GalacticAge {
     this.terrestrialAge = terrestrialAge;
   }
 
-  mercurialAge() {
+  // planetaryYears() {
+  //   let planetRates = [4.17,1.61,0.53,0.08];
+  //   this.planetaryAges = planetRates.map((element) => {
+  //     return parseInt(element * this.terrestrialAge).toFixed(0);
+  //   });
+  // }
+  
+  mercurialYears() {
     let mercurialAge = parseInt((this.terrestrialAge*4.17).toFixed(0));
     return mercurialAge;
   }
 
-  venusianAge() {
+  venusianYears() {
     let venusianAge = parseInt((this.terrestrialAge*1.61).toFixed(0));
     return venusianAge;
   }
 
-  martianAge() {
+  martianYears() {
     let martianAge = parseInt((this.terrestrialAge*0.53).toFixed(0));
     return martianAge;
   }
 
-  jovianAge() {
+  jovianYears() {
     let jovianAge = parseInt((this.terrestrialAge*0.08).toFixed(0));
     return jovianAge;
   }
 
   terrestrialYearsLeft() {
-    let terrestrialYearsLeft = 72-this.terrestrialAge;
-    return terrestrialYearsLeft;
+    this.terrestrialYearsLeft = 72-this.terrestrialAge;
   }
 
 }
